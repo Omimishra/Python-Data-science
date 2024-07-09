96e0f4f293feda5b4319414a8a45245a97fcaff1
Omi Mishra <work.omimishra@gmail.com>
2024-07-09 19:04:37 +0530

    #7 list basics,indexing,slicing,adding

--- pattern5.py
+++ pattern5.py
@@ -1,13 +1,22 @@
-from turtle import *
-colors = ['purple','pink']
-pencolor('beige')
-pensize (3)
-for i in range (6,0,-1):
-    penup()
-    setpos(0,-20*i)
-    pendown()
-    fillcolor(colors[i%2])
-    begin_fill()
-    circle(20*i)
-    end_fill()
-mainloop()
+# from turtle import *
+# colors = ['purple','pink']
+# pencolor('beige')
+# pensize (3)
+# for i in range (6,0,-1):
+#     penup()
+#     setpos(0,-20*i)
+#     pendown()
+#     fillcolor(colors[i%2])
+#     begin_fill()
+#     circle(20*i)
+#     end_fill()
+# mainloop()
+num = int(input('enter a number'))
+i = 2
+while (i< num):
+    if(num % i == 0 ):
+        print('not prime')
+        i=i+1
+        break
+else:
+     print('Number is prime')
