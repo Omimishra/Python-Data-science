6a039dc95103d36e41cc394054c7434b5b1fb31d
Omi Mishra <work.omimishra@gmail.com>
2024-08-02 19:02:34 +0530

    # 19 working on streamlit

---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+
+#ui configuration
+st.set_page_config(
+    page_title='Pokemon App',
+    page_icon='🐰',
+    layout='wide'
+)
+
+# load data
+@st.cache_data
+def load_data():
+    return pd.read_csv('Pokemon.csv' , index_col='#')
+
+#ui integration
+with st.spinner('Loading dataset ....'):
+    df = load_data()
+    # st.balloons()
+
+st.title('Pokemon Data Analytics')
+st.subheader('A simple data app to analyze Pokemon data')
+
+st.sidebar.title('Menu')
+choice = st.sidebar.radio('Options',['View Data','Visualize Data'])
+
+if choice == 'View Data':
+    st.header("View Dataset")
+    st.dataframe(df)
+
+else:
+    st.header('Visualization')
+    # scol = st.sidebar.radio('Select a column', df.columns)
+    # st.write(f'### Visualizing {scol}')
+
+    # if df[scol].dtype == 'object':
+    #     Total_unique_values = df[scol].nunique()
+    #     st.write(f'Total unique values : {Total_unique_values}')
+    # elif df[scol].dtype == 'int64':
+    #     st.write(f'Min value : {df[scol].min()}')
+    #     st.write(f'Max value : {df[scol].max()}')
+    #     st.write(f'Mean value : {df[scol].mean()}')
+    cat_cols = df.select_dtypes(include='object').columns.tolist()
+    num_cols = df.select_dtypes(exclude='object').columns.tolist()
+    cat_cols.remove('Name')
+    num_cols.remove('Generation')
+    num_cols.remove('Legendary')
+    cat_cols.append('Generation')
+    cat_cols.append('Legendary')
+
+
+    snum_col = st.sidebar.selectbox('Select a numeric column',num_cols)
+    scat_col = st.sidebar.selectbox('Select a categorical column',cat_cols)
+
+    c1 , c2 = st.columns(2)
+    #visualize numerical columns
+    fig1 = px.histogram(
+        df,
+        x=snum_col,
+        title=f'Distribution of {snum_col}'
+    )
+
+    #visualize categorical columns
+    fig2 = px.pie(
+        df,
+        names=scat_col,
+        title=f'Distribution of {scat_col}',
+        hole=0.3
+    )
+
+    c1.plotly_chart(fig1)
+    c2.plotly_chart(fig2)
+
+    fig3 = px.box(df, x=scat_col, y=snum_col,title=f'{scat_col} by {snum_col}')
+    st.plotly_chart(fig3)
+
+    fig4 = px.treemap(
+        df,
+        path=['Generation','Type 1'],
+        title=f'Pokemon Type distribution'
+    )
+    st.plotly_chart(fig4)
+
+# to run this program , open terminal and run the following command
+#streamlit run app.py
