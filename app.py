adba2373d562dce505a1c34788646951bd77d623
Omi Mishra <work.omimishra@gmail.com>
2024-08-07 19:03:45 +0530

    # 20 Open CV

--- app.py
+++ app.py
@@ -19,17 +19,17 @@ with st.spinner('Loading dataset ....'):
     df = load_data()
     # st.balloons()
 
-st.title('Pokemon Data Analytics')
+fig = st.title('Pokemon Data Analytics')
 st.subheader('A simple data app to analyze Pokemon data')
-
 st.sidebar.title('Menu')
-choice = st.sidebar.radio('Options',['View Data','Visualize Data'])
+choice = st.sidebar.radio('Options',['View Data','Visualize Data','Column Analysis'])
+
 
 if choice == 'View Data':
     st.header("View Dataset")
     st.dataframe(df)
 
-else:
+elif choice == 'Visualize Data':
     st.header('Visualization')
     # scol = st.sidebar.radio('Select a column', df.columns)
     # st.write(f'### Visualizing {scol}')
@@ -81,6 +81,54 @@ else:
         title=f'Pokemon Type distribution'
     )
     st.plotly_chart(fig4)
+
+elif choice == 'Column Analysis':
+    columns = df.columns.tolist()
+    scol = st.sidebar.selectbox("Select a Column",columns)
+    if df[scol].dtype == 'object':
+        vc = df[scol].value_counts()
+        most_common = vc.idxmax()
+        c1 , c2 = st.columns([3,1])
+        #visualize
+
+        fig = px.funnel_area(names=vc.index,values=vc.values)
+        c1.plotly_chart(fig)
+
+        #value counts 
+        c2.subheader('Total Data')
+        c2.dataframe(vc,use_container_width=True)
+        c2.metric("Most Common ", most_common,int (vc[most_common]))
+        c1,c2 = st.columns(2)
+        fig2 = px.pie(df,names=scol,title= f'Percentage wise of {scol}', hole=0.3)
+        c1.plotly_chart(fig2)
+        fig3 = px.box(df,x=scol,title= f'{scol} by {scol}')
+        c2.plotly_chart(fig3)
+        fig4 = px.funnel_area(names=vc.index,
+                              values=vc.values,
+                              title= f'{scol} Funnel Area',
+                              height=600)
+        st.plotly_chart(fig4,use_container_width=True)
+    else:
+        tab1 , tab2= st.tabs(['Univariate','Bivariate'])
+        with tab1:
+            score = df[scol].describe()
+            fig1 = px.histogram(df,x=scol,title=f'Distribution of {scol}')
+            fig2 = px.box(df, x= scol,title=f'{scol} by {scol}')
+            c1,c2,c3 = st.columns([2,3,3])
+            c1.dataframe(score)
+            c2.plotly_chart(fig1)
+            c3.plotly_chart(fig2)
+        
+        with tab2:
+            c1,c2 = st.columns(2)
+            col2 = c1.selectbox('Select a column ', df.select_dtypes(include='number').columns.tolist())
+            color = c2.selectbox('Select a color ', df.select_dtypes(exclude='number').columns.tolist())
+            fig3 = px.scatter(df,x = scol,y=col2,
+                              color=color,
+                              title=f'{scol} vs {col2}', height=600)
+            st.plotly_chart(fig3,use_container_width=True)
+            
+       
 
 # to run this program , open terminal and run the following command
 #streamlit run app.py
